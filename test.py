0295d1a838300d469f28cd0495f5a6ac37c6f836
pkam-theAmbitious <pakam103@hhu.de>
2026-02-07 21:35:45 +0000

    Fixed Typo

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 import os
 from pathlib import Path
+import json
 
 localvariable1=os.environ.get("REPO_FULL_NAME")
 print(localvariable1)
+
+event_path = os.environ.get("GITHUB_EVENT_PATH")
+
+if not event_path:
+    raise RuntimeError("GITHUB_EVENT_PATH ist nicht gesetzt")
+
+with open(event_path, "r") as f:
+    event = json.load(f)
+
+client_payload = event.get("client_payload", {})
+
+rank = client_payload.get("rank")
+
+print("Client Payload:", client_payload)
+print("Rank:", rank)
+
 def tree(path: Path, prefix=""):
     contents = sorted(path.iterdir(), key=lambda p: (p.is_file(), p.name.lower()))
     for i, item in enumerate(contents):
