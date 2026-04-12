66378a7403e91ba52299d842d587826ba51ca6bc
pkam-theAmbitious <pakam103@hhu.de>
2026-04-12 19:31:56 +0000

    Added data visualization for workflow summary and updated dependencies in `requirements.txt`.

--- report_creator.py
+++ report_creator.py
@@ -100,6 +100,8 @@ def check_for_files(repo_requirements,required_binder,root_files,extended_files)
     if match is not None:
         extended_files.remove(match)
         root_files.append("postbuild")
+    print(f"Root files are: {root_files}")
+    print(f"Extended files are: {extended_files}")
     formal_files = check_for_formal_files(repo_requirements, root_files)
     binder_files=check_for_binder_files(required_binder,extended_files)
     passed=formal_files.passed and binder_files.passed
