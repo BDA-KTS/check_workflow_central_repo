d29e507bc436602467fab7548af3941fb4bc6215
pkam-theAmbitious <pakam103@hhu.de>
2026-05-03 19:11:58 +0000

    Updated test workflow to align file paths with `review_outputs` structure.

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ class PublicSettings:
         "Contact Details": "https://github.com/GESIS-Methods-Hub/guidelines/blob/v0/method/template.md#contact-detailsn "}
     BINDER_DIRS: list[str] = ["", ".binder", "binder"]
     FREE_LICENSES: list[str] = ["Apache-2.0", "MIT", "BSD-2-Clause", "BSD-3-Clause", "ISC", "Zlib", "BSL-1.0", "GNU",
-                                "GPL-3.0","CCO-1.0","AGPL-3.0","EUPL-1.1","EUPL-1.2","LGPL-2.1"]
+                                "GPL-3.0","CC0-1.0","AGPL-3.0","EUPL-1.1","EUPL-1.2","LGPL-2.1"]
     REPO_REQUIREMENTS: dict = {
         "citation": "citation missing: https://github.com/GESIS-Methods-Hub/guidelines?tab=readme-ov-file#quality-criteria",
         "license": "license missing: https://github.com/GESIS-Methods-Hub/guidelines?tab=readme-ov-file#quality-criteria",
